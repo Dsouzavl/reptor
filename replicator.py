f06b874bf0b2edcb5bf6e6ba59712369380431f7
Bernardo Donadio <bdonadio@stone.com.br>
2017-07-05 22:05:18 -0300

    Fix some things :P

--- replicator.py
+++ replicator.py
@@ -1,19 +1,13 @@
 from sys import argv
+from shutil import copyfile
 
 import os
 import random
 
-script = argv
-name = str(script[0])
-cmd = 'start payload.txt'
-foldername = str(random.randint(0,100)*10)
-os.system(cmd)
+name = str(argv[0])
+foldername = "clone"+str(random.randint(0,100)*10)
 
-try:
-	os.mkdir('clone')
-	os.system("copy payload.txt clone")
-	os.system(r"copy " + name + " clone")
-except:
-	os.mkdir('clone' + foldername)
-	os.system("copy payload.txt clone" + foldername)
-	os.system(r"copy " + name + " clone" + foldername)
+
+os.mkdir(foldername)
+copyfile("payload.txt", foldername + '/' + "payload.txt")
+copyfile(name,foldername + '/' + name)
